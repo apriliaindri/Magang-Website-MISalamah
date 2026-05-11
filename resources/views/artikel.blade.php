09d7e235922532de1dc362d468474a23f3cb0bc5
ApriliaIndri <apriliaindryn@gmail.com>
2026-05-11 08:44:59 +0700

    WIP: cleanup and update project

--- resources/views/artikel.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div style="padding:60px; text-align:center;">
-    <h1>Artikel</h1>
-    <p>Daftar artikel sekolah.</p>
-</div>
-@endsection
--- resources/views/tentang.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div style="padding:60px; text-align:center;">
-    <h1>Tentang Kami</h1>
-    <p>Ini adalah halaman tentang sekolah.</p>
-</div>
-@endsection
